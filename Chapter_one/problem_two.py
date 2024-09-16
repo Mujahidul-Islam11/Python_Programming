d1b95905017a73634d078e04768b06ccb9ce0d89
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-16 15:19:24 +0600

    Problem two solved

---
+++ Chapter_one/problem_two.py
@@ -0,0 +1,7 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices') 
+engine.setProperty('voice', voices[1].id)
+engine.say("Hey, I am here with my latest food ordering platform ui. Just have completed the front-end also will create a back-end server for real time data implementing")
+engine.runAndWait()
